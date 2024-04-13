863dc377e023d2895048b20c0deffad50f743219
javiercode <javier.azote@gmail.com>
2024-04-13 05:43:15 -0400

    instancias

--- src/main/java/ubilapaz/edu/bo/template/web/TestController.java
+++ src/main/java/ubilapaz/edu/bo/template/web/TestController.java
@@ -3,7 +3,13 @@ package ubilapaz.edu.bo.template.web;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ubilapaz.edu.bo.template.dto.Response;
+import ubilapaz.edu.bo.template.dto.User;
+import ubilapaz.edu.bo.template.dto.Usuario;
+
+import java.time.LocalDateTime;
 
 @RestController
 public class TestController {
@@ -12,4 +18,40 @@ public class TestController {
     public ResponseEntity<String> test(){
         return new ResponseEntity<String>("Hola mundo!!", HttpStatus.OK);
     }
+
+    @PostMapping("/test")
+    public ResponseEntity<Usuario> dto(){
+        //Usuario
+        //nombre: Pablo
+        //usuario: pgarcia
+        //estodo: true
+        Usuario user = new Usuario();
+        user.setNombre("Pablo");
+        user.setUsuario("pgarcia");
+        user.setEstado(true);
+        user.setFechaRegistro(LocalDateTime.now());
+        return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+    }
+
+    @GetMapping("/testDto")
+    public ResponseEntity<Response> testDto(){
+        Response response = new Response();
+        response.setEstado(true);
+        response.setMensaje("exito<");
+
+        Usuario u = new Usuario();
+        u.setNombre("pablo");
+        return new ResponseEntity<Response>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/dto")
+    public ResponseEntity<User> dtoGet(){
+        User u = new User();
+        u.setNombre("javier");
+        u.setUsername("username.javier");
+
+        return new ResponseEntity<User>(u, HttpStatus.OK);
+    }
+
+
 }
